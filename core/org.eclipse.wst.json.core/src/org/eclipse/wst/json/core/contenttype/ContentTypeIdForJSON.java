e7a851c27f0a7ffb299e966c595d008300961ac2
angelozerr <angelo.zerr@gmail.com>
2015-02-21 12:24:16 +0100

    Add comments

--- core/org.eclipse.wst.json.core/src/org/eclipse/wst/json/core/contenttype/ContentTypeIdForJSON.java
+++ core/org.eclipse.wst.json.core/src/org/eclipse/wst/json/core/contenttype/ContentTypeIdForJSON.java
@@ -1,3 +1,14 @@
+/**
+ *  Copyright (c) 2015-present Angelo ZERR.
+ *  
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
 package org.eclipse.wst.json.core.contenttype;
 
 /**
--- core/org.eclipse.wst.json.core/src/org/eclipse/wst/json/core/document/IJSONModel.java
+++ core/org.eclipse.wst.json.core/src/org/eclipse/wst/json/core/document/IJSONModel.java
@@ -1,7 +1,22 @@
+/**
+ *  Copyright (c) 2015-present Angelo ZERR.
+ *  
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
 package org.eclipse.wst.json.core.document;
 
 import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
 
+/**
+ * The SSE JSON model API.
+ *
+ */
 public interface IJSONModel extends IStructuredModel {
 
 	/**
