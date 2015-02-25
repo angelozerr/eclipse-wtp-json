b9d43146fcc15f635d5995240ad00e0400762663
angelozerr <angelo.zerr@gmail.com>
2015-02-25 23:16:22 +0100

    Add JSON SYntax Color preferences page.

--- core/org.eclipse.wst.json.ui/src/org/eclipse/wst/json/ui/internal/JSONUIMessages.java
+++ core/org.eclipse.wst.json.ui/src/org/eclipse/wst/json/ui/internal/JSONUIMessages.java
@@ -8,10 +8,30 @@ import org.eclipse.osgi.util.NLS;
 public class JSONUIMessages extends NLS {
 
 	private static final String BUNDLE_NAME = "org.eclipse.wst.json.ui.internal.JSONUIMessages";//$NON-NLS-1$
+
 	private static ResourceBundle fResourceBundle;
 
 	public static String refreshoutline_0;
+
+	public static String Sample_JSON_doc;
+	public static String SyntaxColoringPage_0;
+	public static String SyntaxColoringPage_2;
+	public static String SyntaxColoringPage_3;
+	public static String SyntaxColoringPage_4;
+	public static String SyntaxColoringPage_5;
+	public static String SyntaxColoringPage_6;
+	public static String CURLY_BRACE_UI_;
+	public static String COLON_UI_;
+	public static String COMMA_UI_;
+	public static String NORMAL_UI_;
+	public static String OBJECT_KEY_UI_;
+	public static String VALUE_BOOLEAN_UI_;
+	public static String VALUE_NULL_UI_;
+	public static String VALUE_NUMBER_UI_;
+	public static String VALUE_STRING_UI_;
 	
+	public static String EmptyFilePreferencePage_0;
+
 	static {
 		// load message values from bundle file
 		NLS.initializeMessages(BUNDLE_NAME, JSONUIMessages.class);
