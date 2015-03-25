2b71ce1c319bd21b506c07c2a8c2ff18bf44d706
angelozerr <angelo.zerr@gmail.com>
2015-03-25 12:49:04 +0100

    Initialize bower + npm editor.

--- core/org.eclipse.wst.json.ui/src/org/eclipse/wst/json/ui/internal/JSONUIMessages.java
+++ core/org.eclipse.wst.json.ui/src/org/eclipse/wst/json/ui/internal/JSONUIMessages.java
@@ -26,6 +26,10 @@ public class JSONUIMessages extends NLS {
 
 	private static ResourceBundle fResourceBundle;
 
+	// Validation
+	public static String Not_an_integer;
+	public static String Missing_integer;
+	
 	// Outline
 	public static String refreshoutline_0;
 	public static String SortAction_0;
@@ -59,19 +63,36 @@ public class JSONUIMessages extends NLS {
 	public static String Missing_end_object;
 	public static String Missing_start_array;
 	public static String Missing_end_array;
+	
 	// Preferences
 	public static String EmptyFilePreferencePage_0;
 	public static String EncodingSettings_0;
 	public static String EncodingSettings_1;
+	
 	// JSONFilesPreferencePage preferences
 	public static String Creating_files;
 	public static String Encoding_desc;
 	public static String Encoding;
 	public static String JSONFilesPreferencePage_ExtensionLabel;
 	public static String JSONFilesPreferencePage_ExtensionError;
-
+	
+	// JSONContentAssistPreferencePage
+	public static String JSONContentAssistPreferencePage_Auto_Activation_UI_;
+	public static String Automatically_make_suggest_UI_;
+	public static String Auto_Activation_Delay;
+	public static String Prompt_when_these_characte_UI_;
+	public static String Suggestion_Strategy;
+	public static String Suggestion_Strategy_Lax;
+	public static String Suggestion_Strategy_Strict;
+	public static String Group_label_Insertion;
+	public static String Insert_single_proposals;
+	public static String JSONContentAssistPreferencePage_Cycling_UI_;
+	
 	// Content assist
 	public static String Content_Assist_not_availab_UI_;
+	
+	
+
 
 	static {
 		// load message values from bundle file
--- core/org.eclipse.wst.json.ui/src/org/eclipse/wst/json/ui/internal/preferences/JSONUIPreferenceNames.java
+++ core/org.eclipse.wst.json.ui/src/org/eclipse/wst/json/ui/internal/preferences/JSONUIPreferenceNames.java
@@ -68,4 +68,68 @@ public class JSONUIPreferenceNames {
 	 * </p>
 	 */
 	public static final String CONTENT_ASSIST_DEFAULT_PAGE_SORT_ORDER = "json_content_assist_default_page_sort_order"; //$NON-NLS-1$
+
+	public final static String SUGGESTION_STRATEGY_VALUE_LAX = "Lax"; //$NON-NLS-1$
+	public final static String SUGGESTION_STRATEGY_VALUE_STRICT = "Strict"; //$NON-NLS-1$
+
+	/**
+	 * A named preference that holds the characters that auto activate code
+	 * assist.
+	 * <p>
+	 * Value is of type <code>String</code>. All characters that trigger auto
+	 * code assist.
+	 * </p>
+	 */
+	public static final String SUGGESTION_STRATEGY = getSuggestionStrategeyKey();
+
+	private static String getSuggestionStrategeyKey() {
+		return "suggestionStrategy";//$NON-NLS-1$
+	}
+
+	/**
+	 * A named preference that controls time before code assist gets auto
+	 * activated.
+	 * <p>
+	 * Value is of type <code>String</code>.
+	 * </p>
+	 */
+	public static final String AUTO_PROPOSE_DELAY = "autoProposeDelay";//$NON-NLS-1$
+
+	/**
+	 * A named preference that controls if code assist gets auto activated.
+	 * <p>
+	 * Value is of type <code>Boolean</code>.
+	 * </p>
+	 */
+	public static final String AUTO_PROPOSE = getAutoProposeKey();
+
+	private static String getAutoProposeKey() {
+		return "autoPropose";//$NON-NLS-1$
+	}
+
+	/**
+	 * A named preference that holds the characters that auto activate code
+	 * assist.
+	 * <p>
+	 * Value is of type <code>String</code>. All characters that trigger auto
+	 * code assist.
+	 * </p>
+	 */
+	public static final String AUTO_PROPOSE_CODE = getAutoProposeCodeKey();
+
+	private static String getAutoProposeCodeKey() {
+		return "autoProposeCode";//$NON-NLS-1$
+	}
+
+	/**
+	 * <p>
+	 * preference key to store the option for auto insertion of single
+	 * suggestions
+	 * </p>
+	 * <p>
+	 * Value is of type <code>boolean</code>
+	 * </p>
+	 */
+	public static final String INSERT_SINGLE_SUGGESTION = "insertSingleSuggestion"; //$NON-NLS-1$
+
 }
