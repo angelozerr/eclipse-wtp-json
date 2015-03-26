971ddeac8a2cb99ca43328648a21efc326d18ef6
angelozerr <angelo.zerr@gmail.com>
2015-03-26 13:29:09 +0100

    Bower completion for dependencies.

--- bower/org.eclipse.wst.json.bower.ui/src/org/eclipse/wst/json/bower/ui/internal/contentassist/BowerDependencyCompletionProposalCollector.java
+++ bower/org.eclipse.wst.json.bower.ui/src/org/eclipse/wst/json/bower/ui/internal/contentassist/BowerDependencyCompletionProposalCollector.java
@@ -1,4 +1,4 @@
-package org.eclipse.wst.json.npm.ui.internal.contentassist;
+package org.eclipse.wst.json.bower.ui.internal.contentassist;
 
 import java.text.MessageFormat;
 
@@ -7,8 +7,8 @@ import org.eclipse.json.provisonnal.com.eclipsesource.json.JsonObject;
 import org.eclipse.json.provisonnal.com.eclipsesource.json.JsonValue;
 import org.eclipse.json.schema.JSONSchemaType;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.wst.json.npm.ui.internal.editor.NPMEditorPluginImageHelper;
-import org.eclipse.wst.json.npm.ui.internal.editor.NPMEditorPluginImages;
+import org.eclipse.wst.json.bower.ui.internal.editor.BowerEditorPluginImageHelper;
+import org.eclipse.wst.json.bower.ui.internal.editor.BowerEditorPluginImages;
 import org.eclipse.wst.json.ui.contentassist.ContentAssistHelper;
 import org.eclipse.wst.json.ui.contentassist.ContentAssistRequest;
 import org.eclipse.wst.json.ui.contentassist.HttpCompletionProposalCollector;
@@ -16,16 +16,16 @@ import org.eclipse.wst.json.ui.contentassist.JSONKeyCompletionProposal;
 import org.eclipse.wst.json.ui.contentassist.JSONRelevanceConstants;
 import org.eclipse.wst.sse.ui.contentassist.CompletionProposalInvocationContext;
 
-public class NPMDependencyCompletionProposalCollector extends
+public class BowerDependencyCompletionProposalCollector extends
 		HttpCompletionProposalCollector {
 
-	public final String searchUrl = "https://typeahead.npmjs.com/search?q={0}";
-	public final String packageUrl = "http://registry.npmjs.org/{0}/latest";
+	public final String searchUrl = "https://bower.herokuapp.com/packages/search/{0}";
+	public final String packageUrl = "https://bower.herokuapp.com/packages/{0}";
 
 	@Override
 	protected String getUrl(ContentAssistRequest contentAssistRequest,
 			TargetType target) {
-		String keyword = contentAssistRequest.getMatchString();
+		String keyword = "a"; //contentAssistRequest.getMatchString();
 		return MessageFormat.format(searchUrl, keyword);
 	}
 
@@ -34,17 +34,18 @@ public class NPMDependencyCompletionProposalCollector extends
 			CompletionProposalInvocationContext context, TargetType target) {
 		if (json.isArray()) {
 			// Ex :
-			// [{"value":"express"},{"value":"lodash"},{"value":"gulp"}]
+			// [{"name":"angular-mocks","url":"git://github.com/angular/bower-angular-mocks.git"},
+			//  {"name":"angular-moment","url":"git://github.com/urish/angular-moment.git"}]
 			String dependency = null;
 			String replacementString = null;
 			JsonArray values = (JsonArray) json;
 			for (JsonValue value : values) {
 				if (value.isObject()) {
-					dependency = ((JsonObject) value).get("value").asString();
+					dependency = ((JsonObject) value).get("name").asString();
 					replacementString = ContentAssistHelper.getRequiredName(
 							dependency, JSONSchemaType.String);
-					Image icon = NPMEditorPluginImageHelper.getInstance()
-							.getImage(NPMEditorPluginImages.IMG_OBJ_NPM);
+					Image icon = BowerEditorPluginImageHelper.getInstance()
+							.getImage(BowerEditorPluginImages.IMG_OBJ_BOWER);
 					JSONKeyCompletionProposal proposal = new JSONKeyCompletionProposal(
 							replacementString,
 							contentAssistRequest.getReplacementBeginPosition(),
---
+++ bower/org.eclipse.wst.json.bower.ui/src/org/eclipse/wst/json/bower/ui/internal/editor/BowerEditorPluginImages.java
@@ -0,0 +1,9 @@
+package org.eclipse.wst.json.bower.ui.internal.editor;
+
+/**
+ * Bundle of most images used by the Bower Source Editor plug-in.
+ */
+public class BowerEditorPluginImages {
+
+	public static final String IMG_OBJ_BOWER = "icons/full/obj16/bower.png"; //$NON-NLS-1$
+}
